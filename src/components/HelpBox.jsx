61cc8c098f76579ee4a894c131c131c3e9fa177d
akshayshejwalneo <aksgithub@gamil.com>
2026-09-05 12:17:07 +0530

    added new ui more changes [skip ci]

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,6 +4,7 @@ import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   console.log("HelpBox rendered with title:", title, "and text:", text);
+  // console.log("HelpBox rendered with title:", title, "and text:", text);
   return (
     <article className="help-box">
       <h2>{title}</h2>
